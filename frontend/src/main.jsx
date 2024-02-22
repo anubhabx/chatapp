83aff6df2318ff2c46fd09580e5f015c5bcd15be
Anubhab Debnath <anubhabdebnath98@gmail.com>
2024-02-23 02:13:39 +0530

    Update import paths for AuthContext and SocketContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
-import { SocketContextProvider } from "./context/SocketContext";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
