df9ad174e5e7df729e3883549cd42fa0adb690a2
Anubhab Debnath <anubhabdebnath98@gmail.com>
2024-02-23 02:09:29 +0530

    Update import paths in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SocketContextProvider } from "./context/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
